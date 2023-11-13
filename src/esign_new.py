e7509f303ccb0e35875ae7418c60aaede00e7c84
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-11-13 14:51:34 +0530

    Add try-except block to handle FileNotFoundError and save signed PDFs

--- src/esign_new.py
+++ src/esign_new.py
@@ -8,14 +8,23 @@ def esign():
     """
     This function signs all PDF files in the 'PDF_Here' directory using a signature image and saves the signed PDFs in the 'PDF_Signed' directory.
     """
-    for filename in os.listdir('PDF_Here'):
-        settings = {
-                "dir_path": os.path.join('PDF_Here', filename),
-                "output": pdf2jpg.convert_pdf2jpg(settings['dir_path'], outputpath='PDF_Signed', pages='ALL'),
-                "background": Image.open(settings['output'][0]['output_jpgfiles'][0]),
-                "image_open": Image.open('./signature.png').convert('RGBA'),
-                "layer": settings['image_open'].resize((800, 800)),
-                "x":450,
-                "y":2100
-        }
-    settings['background'].paste(settings['layer'], (settings['x'], settings['y']), settings['layer'])
+    try:
+        for filename in os.listdir('PDF_Here'):
+            settings = {
+                    "dir_path": os.path.join('PDF_Here', filename),
+                    "output": pdf2jpg.convert_pdf2jpg(settings['dir_path'], outputpath='PDF_Signed', pages='ALL'),
+                    "background": Image.open(settings['output'][0]['output_jpgfiles'][0]),
+                    "image_open": Image.open('./signature.png').convert('RGBA'),
+                    "layer": settings['image_open'].resize((800, 800)),
+                    "x":450,
+                    "y":2100
+            }
+        settings['background'].paste(settings['layer'], (settings['x'], settings['y']), settings['layer'])
+        settings['background'].save(settings['output'][0]['output_jpgfiles'][0])
+    except FileNotFoundError:
+        with open('log.txt', 'a', encoding='utf-8') as f:
+            f.write("Error: No PDF files found.\n"
+            "Please place the PDF files in the 'PDF_Here' directory.\n")
+        time.sleep(5)
+        exit(0)
+        
